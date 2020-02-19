4ad3ed656704e54905f9238516c53a5ac505084c
Yanisse M. Rodriguez Torres <yanisse.rodriguez@upr.edu>
2020-02-18 22:39:03 -0600

    Integrated Asma's code

--- FRONTEND/newFrontEnd/apTime/src/app/app.module.ts
+++ FRONTEND/newFrontEnd/apTime/src/app/app.module.ts
@@ -6,26 +6,30 @@ import { AuthComponent } from "./auth/auth.component";
 import { ProfileComponent } from "./profile/profile.component";
 import { SecureComponent } from "./secure/secure.component";
 import { HomeComponent } from "./home/home.component";
+import { TasksComponent } from './tasks/tasks.component';
+
+
 import { AmplifyAngularModule, AmplifyService } from "aws-amplify-angular";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 //import UI from "@aws-amplify/ui";
 import { HttpClientModule } from '@angular/common/http';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { CustomMaterialModule } from "./modules/material.module";
 
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import { TasksComponent } from './tasks/tasks.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material';
+//import {MatToolbarModule} from '@angular/material/toolbar';
+//import {MatIconModule} from '@angular/material/icon';
+//import {MatDialogModule} from '@angular/material/dialog';
+//import {MatFormFieldModule} from '@angular/material/form-field';
+
+//import { FormsModule } from '@angular/forms';
+//import {MatButtonModule} from '@angular/material/button';
+//import {MatInputModule} from '@angular/material';
 import {MatMenuModule} from '@angular/material/menu';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material';
+//import {MatCardModule} from '@angular/material/card';
+//import {MatDividerModule} from '@angular/material/divider';
+//import {MatDatepickerModule} from '@angular/material/datepicker';
+//import {MatNativeDateModule} from '@angular/material';
 
 
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -35,6 +39,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { DialogOverviewExampleDialog } from './tasks/tasks.component';
 import { ConfirmDeleteDialog } from './tasks/tasks.component';
 import { EditTaskDialog } from './tasks/tasks.component';
+import { TaskCategoryComponent } from './task-category/task-category.component';
 
 
 @NgModule({
@@ -47,29 +52,20 @@ import { EditTaskDialog } from './tasks/tasks.component';
     TasksComponent,
     DialogOverviewExampleDialog,
     ConfirmDeleteDialog,
-    EditTaskDialog
+    EditTaskDialog,
+    TaskCategoryComponent
   ],
   imports: [BrowserModule, 
   AppRoutingModule, 
   AmplifyAngularModule, 
   BrowserAnimationsModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatDialogModule,
-  MatFormFieldModule,
-  FormsModule,
-  MatButtonModule,
-  MatInputModule,
   FlexLayoutModule,
-  MatMenuModule,
-  MatCardModule,
-  MatDividerModule,
   HttpClientModule,
-  MatDatepickerModule,
-  MatNativeDateModule
+  MatMenuModule,
+  CustomMaterialModule
   ],
   providers: [AmplifyService,
-  MatDatepickerModule
+  CustomMaterialModule
   ],
   bootstrap: [AppComponent],
   entryComponents: [
