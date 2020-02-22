b3e256803b49ac6ca094cf508b56a2944cb2fe90
Bashiir Mohamed <Bashiir.Mire@gmail.com>
2020-02-22 11:00:58 -0600

    updated task model

--- BACKEND/userAuth/src/main/java/com/apptime/auth/model/Task.java
+++ BACKEND/userAuth/src/main/java/com/apptime/auth/model/Task.java
@@ -1,60 +1,4 @@
-Last login: Fri Feb 21 22:42:28 on ttys002
-%                                                                                                                                                      python2018@Bashiirs-MacBook-Pro TimeMgt_Group4_SWENG894 % git status
-On branch TestMaster
-Your branch is up to date with 'origin/TestMaster'.
-
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	modified:   BACKEND/userAuth/src/main/java/com/apptime/auth/controller/TaskManager.java
-	modified:   BACKEND/userAuth/src/main/java/com/apptime/auth/model/Task.java
-	modified:   FRONTEND/newFrontEnd/.DS_Store
-	modified:   FRONTEND/newFrontEnd/apTime/src/app/tasks/tasks.component.html
-	modified:   FRONTEND/newFrontEnd/apTime/src/app/tasks/tasks.component.ts
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-	BACKEND/workspace/
-
-no changes added to commit (use "git add" and/or "git commit -a")
-python2018@Bashiirs-MacBook-Pro TimeMgt_Group4_SWENG894 % git stash
-Saved working directory and index state WIP on TestMaster: 12333310 Merge pull request #18 from lchavarria0D30/Yanisse
-python2018@Bashiirs-MacBook-Pro TimeMgt_Group4_SWENG894 % git checkout Bashiir
-Branch 'Bashiir' set up to track remote branch 'Bashiir' from 'origin'.
 package com.apptime.auth.model;
-
-
-import java.time.LocalDateTime;
-import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-
-@Entity
-public class Task {
-
-        @Id
-        @GeneratedValue
-        private long id;
-        private String name;
-          @JsonFormat(pattern="MM-dd-yyyy HH:mm:ss")
-        private LocalDateTime scheduledstart;
-        @Column(nullable = true)
-          private LocalDateTime duration;
-          @Column(nullable = true)
-          private LocalDateTime scheduledEndTime;
-        private String userName;
-        private String Description;
-
-          public LocalDateTime getScheduledstart() {
-                return scheduledstart;
--- INSERT --package com.apptime.auth.model;
 
 
 import java.time.LocalDateTime;
@@ -76,19 +20,20 @@ public class Task {
 	@GeneratedValue
 	private long id;
 	private String name;
-	  @JsonFormat(pattern="MM-dd-yyyy HH:mm:ss")
-	private LocalDateTime scheduledstart;
+	  //@JsonFormat(pattern="MM-dd-yyyy HH:mm:ss")
+	//private Date schedule ; 
+	private Date scheduledstart;
 	@Column(nullable = true)
 	  private LocalDateTime duration;
 	  @Column(nullable = true)
-	  private LocalDateTime scheduledEndTime;
+	  private LocalDateTime scheduledend;
 	private String userName;
 	private String Description; 
 	
-	  public LocalDateTime getScheduledstart() {
+	  public Date getScheduledstart() {
 		return scheduledstart;
 	}
-	public void setScheduledstart(LocalDateTime scheduledstart) {
+	public void setScheduledstart(Date scheduledstart) {
 		this.scheduledstart = scheduledstart;
 	}
 	
