a54b3fa272d0cfbcd6f700e2990445d97f2ce4c1
Husna (US) <Asma.U.Husna@boeing.com>
2020-02-28 20:16:18 -0600

    added Task Category Private and Public, user feedback

--- FRONTEND/newFrontEnd/apTime/src/app/component/task-category/task-category.component.ts
+++ FRONTEND/newFrontEnd/apTime/src/app/component/task-category/task-category.component.ts
@@ -9,6 +9,10 @@ import { AmplifyService } from 'aws-amplify-angular';
 import { HttpHeaders } from '@angular/common/http';
 import Auth from '@aws-amplify/auth';
 import { SessionService } from '../services/session.service';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FlexAlignStyleBuilder } from '@angular/flex-layout';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-task-category',
   templateUrl: './task-category.component.html',
@@ -21,6 +25,7 @@ export class TaskCategoryComponent implements OnInit {
   status: string;
   amplifyService: any;
   canCreateTask = false;
+  isPublic = false;
   headers = {
     Authorization: 'Bearer'
   };
@@ -30,7 +35,9 @@ export class TaskCategoryComponent implements OnInit {
     private catSevice: CategoryService,
     private amplify: AmplifyService,
     private http: HttpClient,
-    private sessionService: SessionService
+    private sessionService: SessionService,
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {
     this.amplifyService = amplify;
     this.amplifyService.authStateChange$.subscribe(authState => {
@@ -40,7 +47,11 @@ export class TaskCategoryComponent implements OnInit {
 
   ngOnInit() {
     this.categoryForm = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.maxLength(80)])
+      name: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(80)
+      ]),
+      isPublic: new FormControl(false, [])
     });
   }
 
@@ -60,27 +71,63 @@ export class TaskCategoryComponent implements OnInit {
 
   private createCategoryOb = categoryFormValue => {
     this.cat = {
-      name: categoryFormValue.name
-      // description: categoryFormValue.description
+      name: categoryFormValue.name,
+      isPublic: this.isPublic
     };
-    let jtoken = async () =>
-      (await Auth.currentSession()).getIdToken().getJwtToken();
+    console.log(this.cat.isPublic);
 
-    console.log(jtoken);
-    let apiUrl = 'http://localhost/8001/category';
-    this.createCat(this.cat);
+    this.createCat();
   };
   //create Category
-  public createCat = body => {
+  public createCat = function() {
     const headers = {
-      Authorization: 'Bearer ' + this.sessionService.getToken()
+      Authorization: 'Bearer ' + this.sessionService.getToken(),
+      'Content-Type': 'application/json'
     };
-
-    this.http
-      .post('http://localhost:8001/category', body, { headers })
-      .subscribe({
-        next: data => console.log(data),
-        error: error => console.error('There was an error!', error)
-      });
+    if (this.cat.isPublic) {
+      console.log(this.cat.isPublic);
+      this.http
+        .post(
+          'http://localhost:8001/category/public',
+          { name: this.cat.name },
+          { headers }
+        )
+        .subscribe({
+          next: data => {
+            console.log(data);
+            this.openSnackBar('Category Created', 'redirecting to home');
+            this.router.navigate(['/']);
+            // this.location.go('/');
+          },
+          error: error => {
+            console.error('There was an error!', error);
+            this.openSnackBar(
+              'Error occured while  creating category',
+              'Try again!'
+            );
+          }
+        });
+    } else {
+      this.http
+        .post(
+          'http://localhost:8001/category',
+          { name: this.cat.name },
+          { headers }
+        )
+        .subscribe({
+          next: data => {
+            console.log(data);
+            this.openSnackBar('Category Created', 'redirecting to home');
+            this.router.navigate(['/']);
+            // this.location.go('/');
+          },
+          error: error => console.error('There was an error!', error)
+        });
+    }
   };
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000
+    });
+  }
 }
