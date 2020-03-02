160467ad8ce4bbe5cc07e50c5b98543834c4d98e
Qi Zhang <qizhang@apple.com>
2020-03-01 18:00:45 -0800

    [TMGP4-165] Analysis backend class: TaskCategoryServiceImpl https://sweng894group4.atlassian.net/browse/TMGP4-165
    Fix issues found by Idea Analyze. Also update the headers with proper use cases

--- BACKEND/userAuth/src/main/java/com/apptime/auth/repository/TaskCategoryRepository.java
+++ BACKEND/userAuth/src/main/java/com/apptime/auth/repository/TaskCategoryRepository.java
@@ -1,7 +1,6 @@
 package com.apptime.auth.repository;
 
 import com.apptime.auth.model.TaskCategory;
-import com.apptime.auth.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,10 +8,9 @@ import java.util.List;
 /**
  * @author Qi Zhang
  * The DAO to access data of Category
+ * Use Cases: TMGP4-43, TMGP4-39
  */
 public interface TaskCategoryRepository extends JpaRepository<TaskCategory, Integer> {
     List<TaskCategory> findByOwner(String owner);
     List<TaskCategory> findByIsPublic(boolean isPublic);
-    //List<TaskCategory> findByOwnerName(Users owner, String name);
-    // List<TaskCategory> findByNameIsPublic(String name, boolean isPublic);
 }
