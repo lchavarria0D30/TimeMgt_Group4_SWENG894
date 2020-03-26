ff7bd73ede62c1dd8fdc8a767f9c051967ce412f
Qi Zhang <qizhang@apple.com>
2020-03-25 22:36:51 -0700

    [TMGP4-284]: [Backend] The system should be able to connect task with categories https://sweng894group4.atlassian.net/browse/TMGP4-284
    The task would have multiple categories
    Fix a bug

--- BACKEND/userAuth/src/main/java/com/apptime/auth/service/TaskManagerService.java
+++ BACKEND/userAuth/src/main/java/com/apptime/auth/service/TaskManagerService.java
@@ -1,13 +1,11 @@
 package com.apptime.auth.service;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.stream.Collectors;
 import com.apptime.auth.config.TaskStateMachine;
 import com.apptime.auth.model.Task;
+import com.apptime.auth.model.TaskCategory;
 import com.apptime.auth.model.TaskState;
+import com.apptime.auth.repository.TaskCategoryRepository;
 import com.apptime.auth.repository.TaskReportRepository;
 import com.apptime.auth.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,25 +31,32 @@ public class TaskManagerService {
 	@Autowired
 	private NotificationService notificationService;
 
+	@Autowired
+	private TaskCategoryRepository categoryRepository;
+
     //view task details
 	//view task details
 	public Task getTask(long id) {
 		return taskRepo.findById(id);
 
 	}
+
 	public Task getTask(long id, String username) {
 		Task temp = taskRepo.findByIdAndUserName(id,username);
 		return temp;
 
 	}
+
 	public Task getTask(TaskState ts, String userName){
 		return taskRepo.findByUserNameAndState(userName,ts);
 
 	}
+
 	//create task
 	public Task createTask(Task task, String user) {
 		task.setUserName(user);
 		TaskStateMachine.CREATE(task);
+		updateCategories(task);
 		System.out.println("Before saving in Db in CreatTask: +"+task.getScheduledstart());
 		taskRepo.save(task);
 		Task task2 = taskRepo.findById(task.getId());
@@ -60,6 +65,42 @@ public class TaskManagerService {
 		return task;
 	}
 
+	private void updateCategories(Task task) {
+		Set<TaskCategory> categories = task.getCategories();
+		if (categories == null || categories.isEmpty()) {
+			return;
+		}
+
+		List<TaskCategory> privateCategories = categoryRepository.findByOwner(task.getUserName());
+		List<TaskCategory> publicCategories = categoryRepository.findByIsPublic(true);
+		Map<Integer, TaskCategory> idCategoryMap = new HashMap<>();
+		Map<String, TaskCategory> nameCategoryMap = new HashMap<>();
+		List<TaskCategory> allAccessibleCategories = new ArrayList<>();
+		if (privateCategories != null) {
+			allAccessibleCategories.addAll(privateCategories);
+		}
+		if (publicCategories != null) {
+			allAccessibleCategories.addAll(publicCategories);
+		}
+		for (TaskCategory cat : allAccessibleCategories) {
+			idCategoryMap.put(cat.getId(), cat);
+			nameCategoryMap.put(cat.getName(), cat);
+		}
+
+		Set<TaskCategory> realCategories = new HashSet<>();
+		for (TaskCategory category : categories) {
+			TaskCategory categoryInDb;
+			if (category.getId() > 0) {
+				categoryInDb = idCategoryMap.get(category.getId());
+			} else {
+				categoryInDb = nameCategoryMap.get(category.getName());
+			}
+			if (categoryInDb != null) {
+				realCategories.add(categoryInDb);
+			}
+		}
+		task.setCategories(realCategories);
+	}
 
 	//update task
 	public Task updateTask(@RequestBody Task task, String username) {
@@ -70,6 +111,7 @@ public class TaskManagerService {
 		task.setId(old.getId());
 		task.setUserName(username);
 		task.setState(old.getState());
+		updateCategories(task);
 		taskRepo.save(task);
 		notificationService.updateNotificationForTask(task);
 		return task;
@@ -93,8 +135,6 @@ public class TaskManagerService {
 	public List<Task> findUserTasks(String user) {
 		return taskRepo.findByUserName(user);
 	}
-
-
 
 	void setNotificationService(NotificationService notificationService) {
 		this.notificationService = notificationService;
@@ -155,7 +195,6 @@ public class TaskManagerService {
 		return ts;
 	}
 
-
     public Set<Task> getTasksStartedLaterThan(Date start, String name) {
 		Set<Task> tasks = taskRepo.getTasksStartedLaterThan(start,name);
 		Set<Task> result = new HashSet<Task>();
@@ -168,8 +207,8 @@ public class TaskManagerService {
 				if (schldDate.equals(stDate)) {
 					result.add(task);
 				}
-				}
 			}
+		}
 		return result;
     }
 }
