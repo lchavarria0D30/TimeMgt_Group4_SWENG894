eeceb35afc349624a926d5278eddd496fc001bb8
lchavarria0D30 <61021710+lchavarria0D30@users.noreply.github.com>
2020-04-05 16:38:33 -0700

    Updated Unit Test File

--- FRONTEND/newFrontEnd/apTime/src/app/components/create-task-dialog/create-task-dialog.component.spec.ts
+++ FRONTEND/newFrontEnd/apTime/src/app/components/create-task-dialog/create-task-dialog.component.spec.ts
@@ -5,14 +5,14 @@
  *  Unit Test - Frontend
  */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatIconModule} from "@angular/material/icon";
+import { MatIconModule} from '@angular/material/icon';
 import { CreateTaskDialogComponent } from './create-task-dialog.component';
-import {CustomMaterialModule} from "../../modules/material.module";
+import {CustomMaterialModule} from '../../modules/material.module';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {AmplifyService} from 'aws-amplify-angular';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import {RouterTestingModule} from "@angular/router/testing";
-import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import {RouterTestingModule} from '@angular/router/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatFormFieldModule,
@@ -24,7 +24,9 @@ import {
   MatRadioModule,
   MatSelectModule
 } from '@angular/material';
-import {DialogData} from "../tasks/tasks.component";
+import {DialogData} from '../tasks/tasks.component';
+import {ConfirmTaskDialogComponent} from '../confirm-task-dialog/confirm-task-dialog.component';
+import {SessionService} from '../../services/session.service';
 
 describe('CreateTaskDialogComponent', () => {
   let component: CreateTaskDialogComponent;
@@ -43,13 +45,14 @@ describe('CreateTaskDialogComponent', () => {
         MatFormFieldModule,
         MatInputModule,
         MatDialogModule,
+
       ],
-      providers: [AmplifyService, HttpClient, {
+      providers: [AmplifyService, HttpClient,  {
         provide: MatDialogRef,
         useValue: {}},
         { provide: MAT_DIALOG_DATA, useValue: {}}],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      declarations: [ CreateTaskDialogComponent ]
+      declarations: [ CreateTaskDialogComponent, ]
     })
     .compileComponents();
   }));
@@ -76,6 +79,13 @@ describe('CreateTaskDialogComponent', () => {
     expect(component.dateConversion).toBeTruthy();
   });
 
+  /**
+  it('testing pauseTask(number) ', () => {
+    // const testCreate = fixture.debugElement.injector.get(ConfirmTaskDialogComponent);
+    fixture.detectChanges();
+    expect(component.changeMinEndDate).toHaveBeenCalled();
+  });
+*/
   it('should be Defined onNoClick', async(() => {
     spyOn(component, 'onNoClick').and.callThrough();
     fixture.whenStable().then(() => {
@@ -91,4 +101,16 @@ describe('CreateTaskDialogComponent', () => {
       expect(component.onYesClick).toHaveBeenCalledTimes(0);
     });
   }));
+
+  it('should be Defined changeMinEndDate', async(() => {
+    spyOn(component, 'changeMinEndDate').and.callThrough();
+    fixture.whenStable().then(() => {
+      expect(component.changeMinEndDate).toBeDefined();
+      expect(component.changeMinEndDate).toHaveBeenCalled();
+    });
+  }));
 });
+
+
+
+
