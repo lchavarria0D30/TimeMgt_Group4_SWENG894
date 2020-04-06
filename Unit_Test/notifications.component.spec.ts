49037abdb898f7f1a0f080f7e259d8e77d51f771
lchavarria0D30 <61021710+lchavarria0D30@users.noreply.github.com>
2020-04-05 18:38:58 -0700

    Updated Unit Test Notifications Component

--- Unit_Test/notifications.component.spec.ts
+++ Unit_Test/notifications.component.spec.ts
@@ -5,15 +5,61 @@
  *  Unit Test - Frontend
  */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatIconModule} from '@angular/material/icon';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {AmplifyService} from 'aws-amplify-angular';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {CUSTOM_ELEMENTS_SCHEMA, DebugElement} from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotificationsComponent } from './notifications.component';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatButtonModule,
+  MatRadioModule,
+  MatSelectModule
+} from '@angular/material';
+import {By} from '@angular/platform-browser';
+import {DeleteTaskDialogComponent} from "../../delete-task-dialog/delete-task-dialog.component";
+import {NotificationsServiceService} from "../notifications-service.service";
+import {SessionService} from "../../../services/session.service";
+import {Router} from "@angular/router";
 
 describe('NotificationsComponent', () => {
   let component: NotificationsComponent;
   let fixture: ComponentFixture<NotificationsComponent>;
+  let de: DebugElement;
+  let cancelButton: DebugElement;
+  let cancelMessage: NotificationsComponent;
+  let route: Router;
+  let note: NotificationsServiceService;
+  let http: HttpClient;
+  let session: SessionService;
 
   beforeEach(async(() => {
+    cancelMessage = new NotificationsComponent( route, note, http, session);
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+        platformBrowserDynamicTesting());
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, BrowserAnimationsModule,
+        RouterTestingModule.withRoutes([]),
+        MatIconModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDialogModule,
+      ],
+      providers: [AmplifyService, HttpClient, {
+        provide: MatDialogRef,
+        useValue: {}
+      },
+        {provide: MAT_DIALOG_DATA, useValue: {}}],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [ NotificationsComponent ]
     })
     .compileComponents();
@@ -23,9 +69,18 @@ describe('NotificationsComponent', () => {
     fixture = TestBed.createComponent(NotificationsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    cancelButton = fixture.debugElement.query(By.css('mat-raised-button'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should test ', () => {
+    component.ngOnInit();
+    expect(component.ngOnInit).toBeTruthy();
+  });
+
+
 });
